1c2b0e222871cfe26a1204c942fcc532b8e67ca9
Fernando Sustaita <fernandojsustaita@gmail.com>
2022-10-18 14:54:31 -0500

    catalogRepository

--- Catalog.Api/Entities/Product.cs
+++ Catalog.Api/Entities/Product.cs
@@ -1,7 +1,11 @@
-﻿namespace Catalog.Api.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Api.Entities
 {
     public class Product
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string Category { get; set; } = null!;
---
+++ Catalog.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogService catalogService;
+
+        public ProductRepository(ICatalogService catalogService)
+        {
+            this.catalogService = catalogService;
+        }
+        public async Task CreateProduct(Product product)
+        {
+            await catalogService.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await catalogService.Products.DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await catalogService.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await catalogService.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await catalogService.Products.ReplaceOneAsync(filter: p => p.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
